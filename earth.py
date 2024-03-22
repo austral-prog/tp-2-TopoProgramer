8c600cbf261be7d160e329def965975a78152f0f
Bruno <116372845+TopoProgramer@users.noreply.github.com>
2024-03-22 12:56:26 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,9 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+
+    print(f"The result of Bangladesh comes first in the dictionary than Barbados is {x<y}.")
+    print(f"The result of Barbados comes first in the dictionary than Bangladesh is {x>y}.")
+
+
+earth()
