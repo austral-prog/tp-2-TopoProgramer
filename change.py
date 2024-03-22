fac70ef5a9c401cbefbdc47140a121dbcf0635ec
Bruno <116372845+TopoProgramer@users.noreply.github.com>
2024-03-22 12:57:29 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,17 @@
 def change():
     expense = 23.75
     money = 100
+    Cuenta = (money-expense)-(int(money-expense))
+
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print()
+    print("Vuelto")
+    print()
+    print("Pesos:")
+    print(int(money-expense))
+    print("Centavos:")
+    print(int(Cuenta * 100))
+    
